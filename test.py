2cc731e12125a88cb444e095dde26e3207217a2a
Marcus Klaas <mail@marcusklaas.nl>
2016-05-15 16:25:20 +0200

    Log changing connections

--- test.py
+++ test.py
@@ -9,33 +9,24 @@ table_name = 'connection_log'
 cur = conn.cursor()
 cur.execute('create table if not exists %s (mac text, timestamp datetime, is_available boolean)' % table_name)
 
-# Select latest states of all known MACs.
-cur.execute('select log.mac, log.is_available from %s as log\
-             join (select mac, max(timestamp) as timestamp from %s group by mac) x\
-             on log.mac = x.mac and log.timestamp = x.timestamp' % (table_name, table_name))
-status_list = [x for x in cur]
+while True:
+    nm = nmap.PortScanner()
+    nm.scan(hosts='192.168.2.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
+    hosts_list = [(nm[x]['vendor'].keys()[0], 1) for x in nm.all_hosts() if len(nm[x]['vendor']) > 0]
 
-print status_list
+    # Select latest states of all known MACs.
+    cur.execute('select log.mac, log.is_available from %s as log\
+                 join (select mac, max(timestamp) as timestamp from %s group by mac) x\
+                 on log.mac = x.mac and log.timestamp = x.timestamp' % (table_name, table_name))
+    status_list = list(cur)
 
-nm = nmap.PortScanner()
-
-nm.scan(hosts='192.168.2.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
-hosts_list = [(x, nm[x]['status']['state'], nm[x]['vendor']) for x in nm.all_hosts()]
-
-print hosts_list
-
-for host, status, vendor in hosts_list:
-    print('{0}:{1}:{2}'.format(host, status, vendor))
-
-    if len(vendor) > 0:
-        mac = vendor.keys()[0]
+    for mac, status in set(hosts_list) - set(status_list):
         cur.execute('insert into %s (mac, timestamp, is_available) values (?, datetime(\'now\'), ?)' % table_name, (mac, 1))
-        conn.commit()
 
-        # for key, value in vendor.items():
-        #     print(key, value)
+    for mac, status in set([x for x in status_list if x[1] == 1]) - set(hosts_list):
+        cur.execute('insert into %s (mac, timestamp, is_available) values (?, datetime(\'now\'), ?)' % table_name, (mac, 0))
 
-    # Check the previous value of 
-
+    conn.commit()
+    print "Finished loop"
 
 conn.close()
