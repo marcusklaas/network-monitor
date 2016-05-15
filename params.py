9cf0725db3a23327f4fe6c43c5738ac93130cd61
Marcus Klaas <mail@marcusklaas.nl>
2016-05-15 22:54:40 +0200

    Prepare phone checker for crontab

--- params.py
+++ params.py
@@ -1,8 +1,7 @@
-subnetwork = '192.168.1.0/24'
-database_path = '~/network-monitor/connection-log.db'
-phone_mac = 'AA:BB:77:FF:11:CC'
-hue_bridge_ip = '192.168.1.100'
+subnetwork = '192.168.2.0/24'
+database_path = '/home/marcusklaas/python/connection-log.db'
+phone_mac = 'A4:E4:B8:61:6C:6E'
+hue_bridge_ip = '192.168.2.1'
 table_name = 'connection_log'
-hue_username = 'aaaaaaaaaaaaaaaaaaaaaaaaaaabbb'
+hue_username = '6e1d2fb1f8c4c4f1d48920313635d7'
 phone_timeout_minutes = 5
-
